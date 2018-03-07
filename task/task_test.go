3e8b6275ee9006f0e8b4d71959c6d15e6ba64772
hiramotoys <hiramoto.ys@gmail.com>
2018-03-08 00:36:40 +0900

    Typo

--- task/task_test.go
+++ task/task_test.go
@@ -11,7 +11,7 @@ func TestGithubClient(t *testing.T) {
 	repo, _, _ := client.Github.Repositories.Get(context.Background(), "hiramotoys", "cookbook-sample")
 	fmt.Println(repo.CreatedAt)
 	if *repo.GitURL != "git://github.com/hiramotoys/cookbook-sample.git" {
-		t.Error("Failed GitURL is not equql.")
+		t.Error("Failed GitURL is not equal.")
 		t.Log("GitURL: ", *repo.GitURL)
 	}
 }
@@ -19,7 +19,7 @@ func TestGithubClient(t *testing.T) {
 func TestGetRepository(t *testing.T) {
 	repo := GetRepository("hiramotoys", "cookbook-sample")
 	if *repo.GitURL != "git://github.com/hiramotoys/cookbook-sample.git" {
-		t.Error("Failed GitURL is not equql.")
+		t.Error("Failed GitURL is not equal.")
 		t.Log("GitURL: ", *repo.GitURL)
 	}
 }
